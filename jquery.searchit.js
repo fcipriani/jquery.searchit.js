a6767f5cfe31efc2834e67420ed4c8bbcf220991
fcipriani <fabriziocip@gmail.com>
2014-02-23 21:01:31 +0100

    Fixed tabs

--- jquery.searchit.js
+++ jquery.searchit.js
@@ -1,172 +1,172 @@
-  (function ( $ ) {
+	(function ( $ ) {
 
-    $.fn.searchit = function( options ) {
+		$.fn.searchit = function( options ) {
 
-      return this.each( function() {
+			return this.each( function() {
 
-        $.fn.searchit.globals = $.fn.searchit.globals ||
-        {
-          counter: 0
-        }
-        var $counter = $.fn.searchit.globals.counter;
+				$.fn.searchit.globals = $.fn.searchit.globals ||
+				{
+					counter: 0
+				}
+				var $counter = $.fn.searchit.globals.counter;
 
-        var $t = $(this);
-        var opts = $.extend( {}, $.fn.searchit.defaults, options );
+				var $t = $(this);
+				var opts = $.extend( {}, $.fn.searchit.defaults, options );
 				var selectedValue = $t.find("option:selected").text();
 
 				// Use provided text fields, one for each select object
-        if (opts.textField && opts.textField.length > 1)
-          opts.textField = $(opts.textField[$counter]);
+				if (opts.textField && opts.textField.length > 1)
+					opts.textField = $(opts.textField[$counter]);
 				// Build a text field if not available
-        if (opts.textField == null) {
-          $t.before("<input type='textbox' id='__searchit" + $counter + "'" +
+				if (opts.textField == null) {
+					$t.before("<input type='textbox' id='__searchit" + $counter + "'" +
 						// gwincr11 suggestion
 						(opts.firstOptionIsDefault ? " value='" + selectedValue + "'" : "") +
 						"><br>");
-          opts.textField = $('#__searchit' + $counter);
-        }          
+					opts.textField = $('#__searchit' + $counter);
+				}          
 
 				// Use provided css class
-        if (opts.textFieldClass)
-          opts.textField.addClass(opts.textFieldClass);
+				if (opts.textFieldClass)
+					opts.textField.addClass(opts.textFieldClass);
 				// Make select nicer
-        if (opts.dropDown) {
-          $t.css("padding", "5px")
-            .css("margin", "-5px -20px -5px -5px");
+				if (opts.dropDown) {
+					$t.css("padding", "5px")
+						.css("margin", "-5px -20px -5px -5px");
 
-          $t.wrap("<div id='__searchitWrapper" + $counter + "' />");
-          opts.wrp = $('#__searchitWrapper' + $counter);
-          opts.wrp.css("display", "inline-block")
-            .css("vertical-align", "top")
-            .css("overflow", "hidden")
-            .css("border", "solid grey 1px")
-            .css("position", "absolute")
-            .hide();
-          if (opts.dropDownClass)
-            opts.wrp.addClass(opts.dropDownClass);
-        } 
+					$t.wrap("<div id='__searchitWrapper" + $counter + "' />");
+					opts.wrp = $('#__searchitWrapper' + $counter);
+					opts.wrp.css("display", "inline-block")
+						.css("vertical-align", "top")
+						.css("overflow", "hidden")
+						.css("border", "solid grey 1px")
+						.css("position", "absolute")
+						.hide();
+					if (opts.dropDownClass)
+						opts.wrp.addClass(opts.dropDownClass);
+				} 
 
-        opts.optionsFiltered = [];
-        opts.optionsCache = [];
+				opts.optionsFiltered = [];
+				opts.optionsCache = [];
 
-        // Save listbox current content
-        $t.find("option").each( function (index) {
-          opts.optionsCache.push(this);
-        });      
+				// Save listbox current content
+				$t.find("option").each( function (index) {
+					opts.optionsCache.push(this);
+				});      
 
-        // Save options 
-        $t.data('opts', opts);
+				// Save options 
+				$t.data('opts', opts);
 
-        // Hook listbox click
-        $t.click( function(event) {
-          _opts($t).textField.val($(this).find(":selected").text());
-          _opts($t).wrp.hide();
-          event.stopPropagation();  
-        });
+				// Hook listbox click
+				$t.click( function(event) {
+					_opts($t).textField.val($(this).find(":selected").text());
+					_opts($t).wrp.hide();
+					event.stopPropagation();  
+				});
 
-        // Hook html page click to close dropdown
-        $("html").click( function() {
-          _opts($t).wrp.hide();
-        });
+				// Hook html page click to close dropdown
+				$("html").click( function() {
+					_opts($t).wrp.hide();
+				});
 
-        // Hook the keyboard and we're done
-        _opts($t).textField.keyup( function (event) {
-          if (event.keyCode == 13) {
-            $(this).val($t.find(":selected").text());
-            _opts($t).wrp.hide();
-            return;
-          }
-          setTimeout(_findElementsInListBox($t, $(this)), 50);
-        })
+				// Hook the keyboard and we're done
+				_opts($t).textField.keyup( function (event) {
+					if (event.keyCode == 13) {
+						$(this).val($t.find(":selected").text());
+						_opts($t).wrp.hide();
+						return;
+					}
+					setTimeout(_findElementsInListBox($t, $(this)), 50);
+				})
 
 				// Incremente counter so we have unique field ids
-        $.fn.searchit.globals.counter++;      
+				$.fn.searchit.globals.counter++;      
 
-      })
+			})
 
 
-      function _findElementsInListBox(lb, txt) {
+			function _findElementsInListBox(lb, txt) {
 
-        if (!lb.is(":visible")) {
-          _showlb(lb);
-        }
-        
-        _opts(lb).optionsFiltered = [];
-        var count = _opts(lb).optionsCache.length;
-        var dropDown = _opts(lb).dropDown;
-        var searchText = txt.val().toLowerCase();
+				if (!lb.is(":visible")) {
+					_showlb(lb);
+				}
+				
+				_opts(lb).optionsFiltered = [];
+				var count = _opts(lb).optionsCache.length;
+				var dropDown = _opts(lb).dropDown;
+				var searchText = txt.val().toLowerCase();
 
-        // find match (just the old classic loop, will make the regexp later)
-        $.each(_opts(lb).optionsCache, function (index, value) {
-          if ($(value).text().toLowerCase().indexOf(searchText) > -1)
-          {
-            // save matching items 
-            _opts(lb).optionsFiltered.push(value);
-          }
+				// find match (just the old classic loop, will make the regexp later)
+				$.each(_opts(lb).optionsCache, function (index, value) {
+					if ($(value).text().toLowerCase().indexOf(searchText) > -1)
+					{
+						// save matching items 
+						_opts(lb).optionsFiltered.push(value);
+					}
 
-          // Trigger a listbox reload at the end of cycle    
-          if (! --count) {
-            _filterListBox(lb);
-          }
-        });
-      }
+					// Trigger a listbox reload at the end of cycle    
+					if (! --count) {
+						_filterListBox(lb);
+					}
+				});
+			}
 
-      function _opts(lb) {
-        return lb.data('opts');
-      }
+			function _opts(lb) {
+				return lb.data('opts');
+			}
 
-      function _showlb(lb) {
-        if (_opts(lb).dropDown) {
-          var tf = _opts(lb).textField;          
-          lb.attr("size", _opts(lb).size);
-          _opts(lb).wrp.show().offset({
-            top: tf.offset().top + tf.outerHeight(), 
-            left: tf.offset().left
-          });
-          _opts(lb).wrp.css("width", tf.outerWidth() + "px");
-          lb.css("width", (tf.outerWidth() + 25) + "px");
-        }
-      }
+			function _showlb(lb) {
+				if (_opts(lb).dropDown) {
+					var tf = _opts(lb).textField;          
+					lb.attr("size", _opts(lb).size);
+					_opts(lb).wrp.show().offset({
+						top: tf.offset().top + tf.outerHeight(), 
+						left: tf.offset().left
+					});
+					_opts(lb).wrp.css("width", tf.outerWidth() + "px");
+					lb.css("width", (tf.outerWidth() + 25) + "px");
+				}
+			}
 
-      function _filterListBox(lb) {
-        lb.empty();
+			function _filterListBox(lb) {
+				lb.empty();
 
-        if (_opts(lb).optionsFiltered.length == 0) {
-          lb.append("<option>" + _opts(lb).noElementText + "</option>");
-        } else {
-          $.each(_opts(lb).optionsFiltered, function (index, value) {
-            lb.append(value);
-          });
-          lb[0].selectedIndex = 0;
-        }
-      }
-    }
+				if (_opts(lb).optionsFiltered.length == 0) {
+					lb.append("<option>" + _opts(lb).noElementText + "</option>");
+				} else {
+					$.each(_opts(lb).optionsFiltered, function (index, value) {
+						lb.append(value);
+					});
+					lb[0].selectedIndex = 0;
+				}
+			}
+		}
 
-    $.fn.searchit.defaults = {
+		$.fn.searchit.defaults = {
 			// Provide custom text fields where select will be attached, must be a jquery object.
 			// If multiple text fields are provided, select objects will be attached to them
 			// in the order they are found
-      textField: null,										
+			textField: null,										
 
 			// Text fields css class
-      textFieldClass: null,								
+			textFieldClass: null,								
 
 			// Show the select object as a custom dropdown under the text fields. If it is false,
 			// the select object will be left where it is, and typing in the text field will
 			// work as a quick select
-      dropDown: true,											
+			dropDown: true,											
 
 			// The dropdown css class
-      dropDownClass: null,
+			dropDownClass: null,
 
 			// Number of the visible options in the select object
-      size: 5,
+			size: 5,
 
 			// Text to show when no element matches the typed text
-      noElementText: "No elements found",
+			noElementText: "No elements found",
 
 			// Show the first select option in the text field
 			showFirstOption: false		
-    }    
+		}    
 
-  }(jQuery))
+	}(jQuery))
